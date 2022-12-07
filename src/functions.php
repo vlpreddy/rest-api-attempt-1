dd10eeb33cfa7939e5289690840e320eba7c07c2
leela Parameswara reddy <info@redmindstech.com>
2022-12-07 12:48:35 +0530

    docker setup

---
+++ src/functions.php
@@ -0,0 +1,11 @@
+<?php
+spl_autoload_register(function($className){
+$path = strtolower($className) . ".php";
+if(file_exists($path)){
+    require_once($path);
+}else {
+    echo "file $path is not found";
+}
+
+});
+?>
---
+++ src/rest.php
@@ -0,0 +1,30 @@
+<?php
+
+class Rest{
+    public function __construct(){
+
+    } 
+
+    public function validateRequest(){
+
+    }
+
+    public  function processApi(){
+
+    }
+
+    public function validateParameter($fieldName, $value, $datatype, $required ){
+
+    }
+
+    public function throwError($code, $message){
+
+    }
+
+    public function returnResponse(){
+
+    }
+}
+
+
+?>
