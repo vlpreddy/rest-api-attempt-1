f1b290a3f60bb422f1bfc9615025582f13e024f6
leela Parameswara reddy <info@redmindstech.com>
2022-12-07 17:33:30 +0530

    succesfull db connection using pdo with error handling

---
+++ src/DbConnect.php
@@ -0,0 +1,26 @@
+
+<?php
+
+class DbConnect {
+    private $server = 'db';
+    private $dbname = 'restapi';
+    private $user = 'root';
+    private $pass = 'example';
+
+
+    public function connect(){
+        try{
+        $conn = new PDO('mysql:host='. $this->server.';dbname='.$this->dbname, $this->user,$this->pass);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+        } catch(Exception $e) {
+            echo "DAtabase Error " . $e->getMessage();
+        }
+    }
+}
+
+$db = new DbConnect;
+$db->connect();
+
+
+?>
